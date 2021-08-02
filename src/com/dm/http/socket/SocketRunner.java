713df8b5689e9b1e945dc8ec9fd27ee50efd6088
Dima <briksindima@gmail.com>
2021-08-02 20:19:19 +0100

    socketrunner implementation

---
+++ src/com/dm/http/socket/SocketRunner.java
@@ -0,0 +1,22 @@
+package com.dm.http.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketRunner {
+    public static void main(String[] args) throws IOException {
+
+        InetAddress inetAddress = Inet4Address.getByName("google.com");
+        try (var socket = new Socket("google.com", 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello world");
+            byte[] response = inputStream.readAllBytes();
+            System.out.println(response.length);
+        }
+    }
+}
