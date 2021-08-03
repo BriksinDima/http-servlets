4ffc414c950d6dd34e72ca963d3aaa47529bd4dc
Dima <briksindima@gmail.com>
2021-08-03 20:19:19 +0100

    client and server sync negotiation

--- src/com/dm/http/socket/SocketRunner.java
+++ src/com/dm/http/socket/SocketRunner.java
@@ -4,18 +4,22 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
 
-        InetAddress inetAddress = Inet4Address.getByName("localhost");
+        var inetAddress = Inet4Address.getByName("localhost");
         try (var socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
-            outputStream.writeUTF("Hello world!");
-            System.out.println("Response from server: " + inputStream.readUTF());
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
         }
     }
 }
--- src/com/dm/http/socket/SocketServerRunner.java
+++ src/com/dm/http/socket/SocketServerRunner.java
@@ -4,16 +4,23 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Scanner;
 
 public class SocketServerRunner {
 
     public static void main(String[] args) throws IOException {
-        try(ServerSocket serverSocket = new ServerSocket(7777);
-            var socket = serverSocket.accept();
-            var dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            var dataInputStream = new DataInputStream(socket.getInputStream())) {
-            System.out.println("Client request " + dataInputStream.readUTF());
-            dataOutputStream.writeUTF("Hello from server!");
+        try (var serverSocket = new ServerSocket(7777);
+             var socket = serverSocket.accept();
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
+            var request = inputStream.readUTF();
+            while (!"stop".equals(request)) {
+                System.out.println("Client request: " + request);
+                var response = scanner.nextLine();
+                outputStream.writeUTF(response);
+                request = inputStream.readUTF();
+            }
         }
     }
 }
